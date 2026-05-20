a3ce727929b6626f18b8b82b5ed1078083aae781
Bhavani-Edhurupaka <bhavaniedhurupaka11@gmail.com>
2026-05-20 14:50:55 +0530

    Add files via upload

---
+++ compiler.py
@@ -0,0 +1,113 @@
+'''a=3
+b=4
+print(a+b)'''
+'''a=4
+b=9
+c=a+b
+print(c)'''
+#print(5+6)
+#run_time input()
+'''a=int(input("enter a value"))
+b=int(input("enter b value"))
+print(a+b)'''
+
+
+'''a=float(input("enter a value"))
+b=float(input("enter b value"))
+print(a+b)'''
+'''fname=str(input("enter a value"))
+lname=str(input("enter b value"))
+print(lname+fname)'''
+'''city=input("city name")
+print("city name is",city)'''
+
+'''a=complex(input())
+b=complex(input())
+print(a+b)'''
+'''a=bool(input("data1"))
+a=bool(i"nput("data2"))
+a=bool(input("data3"))
+print(a+a+a)'''
+'''a=int(input("a value"))
+b=int(input("a value"))
+c=input("add,sub,mul,div")
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)'''
+
+'''a=int(input("a value"))
+b=int(input("a value"))
+c=int(input("1,2,3,4"))
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)'''
+'''a=int(input("a value"))
+b=int(input("a value"))
+c=input(add,
+        sub,
+        mul,
+        div)
+print(a+b)
+print(a-b)'''
+#TASKS
+'''a=["codegnan","python","course"]
+b=str(a)
+c=b.upper()
+print(c)'''
+
+'''a=input("enter student name")
+b=input("enter student id")
+c=input("enter student mobile")
+d=input("enter student email")
+e=input("enter student dob")
+f=input("enter student city")
+g=input("enter student address")
+print(".....student profile.....")
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)'''
+#TASKS 
+'''a=[7,9,10,11,12,15]
+b=a.append("python")
+print(b)'''
+'''a=[10,20,30,40,50,60]
+b=a.extend("python")
+print(b)'''
+'''a=[2,7,8,9,10]
+b=a.insert(1,5)
+c=tuple(b)
+print(c)'''
+'''a={4,6,8,9,10,11}
+b=a.pop()
+print(b)'''
+'''a=(5,6,7,8,9,10)
+b=list(a)
+c=b.reverese()
+d=tuple(c)
+print(d)'''
+'''a={"year":2024,"month":5}
+b=a.update({"date":3})
+print(b)'''
+a={"studentname":["sreshta","divya","tulasi"],"subject":["math","science","social"],"percentage":[78,87,90]}
+print(a)
+
+
+
+
+
+
+      
+
+
+
+
+
+
+
+
